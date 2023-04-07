454a0d6824f5d528b5cc711beb3f04780c90b981
julienMP06 <102045381+julienMP06@users.noreply.github.com>
2023-04-07 13:43:34 +0000

    MAJ

---
+++ src/MainSceneController.java
@@ -0,0 +1,24 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainSceneController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void PenButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module ProjectDrawApp {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
