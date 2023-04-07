f1c7a13e80d1cd13223b71580f1570b4ed6c72cc
gui2L <gsurleau@gmail.com>
2023-04-07 20:29:32 +0200

    dessiner_PremierTest

--- src/MainSceneController.java
+++ src/MainSceneController.java
@@ -2,8 +2,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.*;
+import javafx.scene.input.MouseEvent;
 
 public class MainSceneController {
+    GraphicsContext gc;
+
+    @FXML 
+    private Canvas board;
 
     @FXML
     private ResourceBundle resources;
@@ -19,6 +25,13 @@ public class MainSceneController {
     @FXML
     void initialize() {
 
+    }
+
+    @FXML
+    void draw(MouseEvent e){
+        gc = board.getGraphicsContext2D();
+        gc.fillOval(e.getX(), e.getY(), 5, 5); // fonction à changer 
+        
     }
 
 }
