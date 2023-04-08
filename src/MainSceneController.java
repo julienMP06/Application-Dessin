ed926388d1071bd0770a2d2d9c59bc52045fe11c
julienMP06 <102045381+julienMP06@users.noreply.github.com>
2023-04-08 11:07:56 +0000

    MAJ

--- src/MainSceneController.java
+++ src/MainSceneController.java
@@ -2,14 +2,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.canvas.*;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 public class MainSceneController {
-    GraphicsContext gc;
 
-    @FXML 
-    private Canvas board;
+    GraphicsContext gc;
 
     @FXML
     private ResourceBundle resources;
@@ -18,20 +19,41 @@ public class MainSceneController {
     private URL location;
 
     @FXML
+    private Canvas board;
+
+    @FXML
+    private Button btnUnZoom;
+
+    @FXML
+    private Button btnZoom;
+
+    @FXML
     void PenButton(ActionEvent event) {
 
     }
 
     @FXML
-    void initialize() {
+    void draw(MouseEvent e) {
+        gc = board.getGraphicsContext2D();
+        gc.setFill(Color.RED);
+        gc.fillOval(e.getX(), e.getY(), 5, 5); // fonction à changer 
+        System.out.println(e.getX() + " " + e.getY());
+    }
+
+    @FXML
+    void handleZoomInButtonAction(ActionEvent event) {
 
     }
 
     @FXML
-    void draw(MouseEvent e){
+    void initialize() {
         gc = board.getGraphicsContext2D();
-        gc.fillOval(e.getX(), e.getY(), 5, 5); // fonction à changer 
-        
+        board.setOnMouseDragged(this::draw);
+        board.setStyle("-fx-background-color: black;");
+        assert board != null : "fx:id=\"board\" was not injected: check your FXML file 'MainScene.fxml'.";
+        assert btnUnZoom != null : "fx:id=\"btnUnZoom\" was not injected: check your FXML file 'MainScene.fxml'.";
+        assert btnZoom != null : "fx:id=\"btnZoom\" was not injected: check your FXML file 'MainScene.fxml'.";
+
     }
 
 }
