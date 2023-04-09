3664420999e120957e82aca19c55e11cec048e67
julienMP06 <102045381+julienMP06@users.noreply.github.com>
2023-04-09 11:09:02 +0000

    Zoom

--- src/App.java
+++ src/App.java
@@ -5,25 +5,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
- 
+
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
-
-        Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
             Scene scene = new Scene(root);
-            scene.getStylesheets().add("style.css");
+            String css = getClass().getResource("style.css").toExternalForm();
+            scene.getStylesheets().add(css);
   
             primaryStage.setTitle("Hello World!");
-                  primaryStage.setScene(scene);
-                  primaryStage.show();
-            } catch (IOException e) {
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.err.println("Erreur lors du chargement de MainScene.fxml : " + e.getMessage());
+            e.printStackTrace();
         }
     }
- 
- public static void main(String[] args) {
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
